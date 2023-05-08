3ff092a24ec4443b90a3d0942688ba2691840f2e
Saurav Kumar <saurav.kumar2@kantar.com>
2023-05-08 19:55:01 +0530

    demo source for the idea of Http2client

--- Http2Client/src/main/java/com/example/demo/Http2ClientExample.java
+++ Http2Client/src/main/java/com/example/demo/Http2ClientExample.java
@@ -14,5 +14,15 @@ public class Http2ClientExample {
 		HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
 		System.out.println(httpResponse.body());
+		/*{
+			  "args": {}, 
+			  "headers": {
+			    "Host": "httpbin.org", 
+			    "User-Agent": "Java-http-client/17.0.6", 
+			    "X-Amzn-Trace-Id": "Root=1-645905da-17ffbec53a034f543554c8ee"
+			  }, 
+			  "origin": "165.85.55.102", 
+			  "url": "https://httpbin.org/get"
+		}*/
 	}
 }
